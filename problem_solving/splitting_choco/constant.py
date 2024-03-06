383519f592af6f7814ace98f1901731b6ba2ad80
jiad <abdulziad08@outlook.com>
2024-03-06 20:09:19 +0100

    .

---
+++ problem_solving/splitting_choco/constant.py
@@ -0,0 +1,8 @@
+def choco(h, w):
+    return (w - 1) + (w * (h - 1))
+
+print(choco(1, 1))
+print(choco(4, 3))
+print(choco(8, 5))
+print(choco(20, 5))
+print(choco(30, 3))
--- problem_solving/splitting_choco/recursion.py
+++ problem_solving/splitting_choco/recursion.py
@@ -2,4 +2,8 @@ def split_choco(h, w):
     if w == 1: return h - 1
     else: return 1 + split_choco(h, w - 1) + split_choco(h, 1)
 
+print(split_choco(1, 1))
 print(split_choco(4, 3))
+print(split_choco(8, 5))
+print(split_choco(20, 5))
+print(split_choco(30, 3))
