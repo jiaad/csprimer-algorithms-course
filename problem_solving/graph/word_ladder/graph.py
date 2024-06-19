6fc08e3594bacb0e395b3c47c2dcbe2cb5965449
jiad <abdulziad08@outlook.com>
2024-06-19 21:32:34 +0200

    word ladder bfs

---
+++ problem_solving/graph/word_ladder/graph.py
@@ -0,0 +1,109 @@
+class Graph:
+    def __init__(self):
+        self.adjacency = {}
+ 
+    # a node is a vertex
+    def add_vertex(self, vrtx_name):
+        if not self.adjacency.get(vrtx_name):
+            self.adjacency[vrtx_name] = []
+
+    def add_edge(self, key1, key2):
+        if key1 in self.adjacency:
+            if not key2 in self.adjacency[key1]:
+                self.adjacency[key1].append(key2)
+        if key2 in self.adjacency:
+            if not key1 in self.adjacency[key2]:
+                self.adjacency[key2].append(key1)
+
+    def remove_edge(self, key1, key2):
+        if key1 in self.adjacency:
+            filtered = filter(lambda x: x != key2, self.adjacency[key1])
+            self.adjacency[key1] = list(filtered) 
+        if key2 in self.adjacency:
+            filtered = filter(lambda x: x != key1, self.adjacency[key2])
+            self.adjacency[key2] = list(filtered)
+    def remove_vertex(self, vrtx):
+        while len(self.adjacency):
+            key = self.adjacency[vrtx].pop()
+            self.remove_edge(vrtx, key)
+        del self.adjacency[vrtx] 
+
+
+    def neighbors(self, vrtx):
+        keys = self.adjacency
+        return keys.get(vrtx, []) 
+
+    def dfs_rec(self, start_vrtx):
+        visited, res = set(), []
+        def dfs(vrtx):
+            if not vrtx: return
+            res.append(vrtx)
+            visited.add(vrtx)
+            
+            for key in self.neighbors(vrtx):
+                if not key in visited:
+                    dfs(key)
+            return res
+
+        return dfs(start_vrtx)
+        
+    def dfs_itter(self, start_vrtx):
+        visited, res, stack = set(), [], [start_vrtx]
+        while len(stack):
+            vertex = stack.pop()
+            if vertex in visited: continue
+            visited.add(vertex)
+            res.append(vertex)
+            for v in self.neighbors(vertex):
+                stack.append(v)
+        return res
+        
+    def bfs(self, start_vrtx):
+        visited, res, q = set(), [], [start_vrtx]
+
+        while len(q):
+            vrtx = q.pop(0)
+            if vrtx in visited: continue
+            res.append(vrtx)
+            visited.add(vrtx)
+            for x in self.neighbors(vrtx):
+                q.append(x)
+
+        return res
+
+
+
+if __name__ == "__main__":
+    gr = Graph()
+    gr.add_vertex("paris")
+    gr.add_vertex("london")
+    gr.add_vertex("milan")
+    gr.add_vertex("tokyo")
+    gr.add_vertex("kyoto")
+    gr.add_edge("paris", "milan")
+    gr.add_edge("london", "milan")
+
+    graph = Graph()
+    graph.add_vertex("A")
+    graph.add_vertex("B")
+    graph.add_vertex("C")
+    graph.add_vertex("D")
+    graph.add_vertex("E")
+    graph.add_vertex("F")
+
+    graph.add_edge("A", "B")
+    graph.add_edge("A", "C")
+    graph.add_edge("B", "D")
+    graph.add_edge("C", "E")
+    graph.add_edge("D", "E")
+    graph.add_edge("D", "F")
+    graph.add_edge("E", "F")
+    print(graph.dfs_rec("A"))
+    print(graph.dfs_itter("A"))
+    print(graph.bfs("A"))
+
+
+    iife = lambda fn: fn()
+    @iife
+    def p():
+        print("hellllooooo")
--- problem_solving/graph/word_ladder/ladder.py
+++ problem_solving/graph/word_ladder/ladder.py
@@ -1,147 +1,52 @@
-import sys
+from collections import defaultdict
+from graph import Graph
+import time
 
-class Edge(object):
-    def __init__(self, src, dst):
-        self.src = src
-        self.dst = dst
-    def __repr__(self):
-        return f'src: {self.src} - dst: {self.dst}'
-        
-    def __str__(self):
-        return f'src: {self.src} - dst: {self.dst}'
 
-class Node(object):
-    def __init__(self, word):
-        self.word = word
-
-class Graph(object):
-    def __init__(self):
-        self.vertices = {}
-    def add(self, key, word):
-        if self.vertices.get(key):
-            self.vertices[key].append(word)
-        else:
-            self.vertices[key] = [word]
-        
-def words_reader():
-    words = {}
-    with open("words.txt", "r") as f:
-        data = f.readline()[0:-2]
-        while data:
-            words[data] = 1
-            data = f.readline()[0:-1]
-
-    return words
-
-def is_word(word, words):
-    return words.get(word)
-
-def gen_combi(word, words):
+def make_graph():
+    key_variations = defaultdict(list)
+    graph = Graph()
     visited = set()
-    res = []
-    for i, x in enumerate(word):
-        for a in alphabet:
-            if a == x: continue
-            current = list(word)
-            current[i] = a
-            current = "".join(current)
-            if current in visited: continue
-            if is_word(current, words):
-                visited.add(current)
-                res.append(current)
-    return res
+    with open("./words.txt") as f:
+        while data := f.readline().strip().lower():
+            if data in visited: continue
+            visited.add(data)
+            for j in range(len(data)):
+                res = "".join("?" if k == j else x for k, x in enumerate(data))
+                key_variations[res].append("".join(data))
+                graph.add_vertex(data)
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-def word_generator(beg_word, end_word) :
-    
+    for x in key_variations:
+        i = 0
+        keys = key_variations[x]
+        while i < len(keys):
+            j = i + 1
+            while j < len(keys):
+                graph.add_edge(keys[i], keys[j])
+                j += 1
+            i += 1
+    return graph
+
+def ladder_bfs(start, end, graph):
+    q = [(start, [start], 0)]
     visited = set()
-    visited.add(beg_word)
-    words = words_reader()
-    
-    def inner(word, stock):
-        if len(stock) and stock[-1].dst == end_word:
-            print("yattttaaaaaaa")
-            return stock
+    while len(q):
+        vrtx, path, depth = q.pop(0)
+        if vrtx == end:
+            return path, depth
+        for x in graph.neighbors(vrtx):
+            if x not in visited:
+                visited.add(x)
+                q.append((x, path + [x], depth + 1))
+    return None
+
         
-        for i, x in enumerate(word):
-            for a in alphabet:
-                if a == x: continue
-                current = list(word)
-                current[i] = a
-                current = "".join(current)
-                if current in visited: continue
-                if is_word(current, words):
-                    visited.add(current)
-                    found = inner(current, stock + [Edge(word, current)])
-                    return found
+if __name__ == "__main__":
+    ladder_graph = make_graph()
+    print(ladder_bfs("hey", "sty", ladder_graph))
+    print(ladder_bfs("aloof", "coins", ladder_graph))
+    print(ladder_bfs("ring", "nice", ladder_graph))
+    print(ladder_bfs("pig", "pit", ladder_graph))
+    print(ladder_bfs("aloof", "piggy", ladder_graph))
+    print(ladder_bfs("hey", "say", ladder_graph))
 
-    #return inner(big_word, [])
-    mem = set()
-    def loop(word):
-        q = [word]
-        gr = {}
-        gr[word] = []
-        done = False
-        keys = [word]
-        while len(q):
-            pop = q.pop()
-            mem.add(pop)
-            if pop == end_word:
-                print("=========================================")
-                return pop
-            generated = gen_combi(pop, words)
-            gr[pop] = generated 
-            keys.append(pop)
-            for x in generated:
-                if x in mem: continue
-                if x == end_word:
-                    print("print", gr)
-                    print("--------------", gr)
-                    print("--------------", keys )
-
-                    return gr
-                q.insert(0, x)
-        print(gr)
-        
-    visited = set()
-
-    def find_node(current, keys, graph, trace):
-        visited.add(current)
-        if end_word in keys: 
-            return 12, trace + [end_word]
-        for x in keys:
-            if not x in visited:
-                res = find_node(x, graph.get(x, []), graph, trace + [x])
-                if res: print("===>", res)
-            else:
-                verify = trace + [x, end_word]
-                is_path = True
-                for i in range(1, len(verify)):
-                    if not is_one_letter_diff(verify[i], verify[i - 1]) :
-                        is_path = False
-                        break
-                if is_path: print("**** this is path", verify)
-    graphed = loop(beg_word)
-    return find_node(beg_word, graphed.get(beg_word), graphed, [beg_word])
-            
-
-
-            
-
-
-words = words_reader()
-def pretty(res):
-    #return [x.src for x in res]
-    return res
-
-def is_one_letter_diff(dst, src):
-    if len(dst) != len(src): return False
-    diff = 0
-    for i, x in enumerate(dst):
-        if x != src[i]:
-            diff += 1
-    return diff == 1
-    
-print(pretty(word_generator("cap", "rip")))
-        
-    
